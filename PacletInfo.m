69c91d788cbcc4245803f6ee63212d0b53352fd9
stephan.sinn <stephan.sinn@kit.edu>
2020-05-10 19:56:34 +0200

    init commit

---
+++ PacletInfo.m
@@ -0,0 +1,11 @@
+(* Paclet Info File *)
+
+(* created 2020.04.27*)
+
+Paclet[
+  Name -> "kineticSimFit",
+  Version -> "0.0.1",
+  MathematicaVersion -> "6+",
+  Extensions -> {
+    {"Documentation", Language -> "English"}
+}]
---
+++ kineticSimFit/kineticSimFit.m
@@ -0,0 +1,16 @@
+(* Wolfram Language Package *)
+
+(* Created by the Wolfram Workbench 27.04.2020 *)
+
+BeginPackage["kineticSimFit`"]
+(* Exported symbols added here with SymbolName::usage *) 
+
+Begin["`Private`"]
+(* Implementation of the package *)
+
+
+
+End[]
+
+EndPackage[]
+
